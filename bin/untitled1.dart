5437ab2ea44cba3a83b6ab3015fdfd47245a0f80
BambangSB <61703343+BambangSB@users.noreply.github.com>
2022-06-29 10:27:13 +0700

    belajar mengubah (parse) tipe data

--- bin/untitled1.dart
+++ bin/untitled1.dart
@@ -2,6 +2,7 @@ import 'package:untitled1/untitled1.dart' as untitled1;
 import 'dart:io';
 
 void main(List<String> arguments) {
+  //============= Belajar variable =====================
   /*var greetings = 'assalamualaikum zakia';
   var umur;
   umur = 22;
@@ -15,25 +16,64 @@ void main(List<String> arguments) {
 
   print(x);
    */
+  //=========================================================
 
+  //============= Belajar input typeface =====================
   /*stdout.write('Masukkan Nama lengkap : ');
   String name = stdin.readLineSync()!;
   stdout.write('Masukkan umur anda : ');
   int age = int.parse(stdin.readLineSync()!);
   print('Nama anda adalah : $name, dan anda memiliki umur : $age');
    */
+  //=========================================================
 
+  //============= Belajar parse tipedata =====================
   // String -> Int
+  /*
   var sebelas = int.parse('11');
 
-//  String -> Double
+  //  String -> Double
   var sebelasKomaDua = double.parse('11.2');
 
-//  Int -> String
+  //  Int -> String
   var sebelasKeString = 11.toString();
 
-//  Double -> String
+  //  Double -> String
   var piKeString = 3.14159.toString(); //String piKeString = 3.14
+  //=========================================================
 
+  //============= Belajar escape string =====================
+  print('"What do you think of Dart?" he asked');
+
+  print('"I think it\'s great!" I answered confidently'); //Backslash sebelum tanda petik hanya tanda patik
+
+  //String Interpolation
+  var name = 'Messi';
+  print('Hello $name, nice to meet you.');
+  print('1 + 1 = ${1 + 1}'); //memasukkan penjumlahan dalam string
+
+  print(r'Dia baru saja membeli komputer seharga $1,000.00'); //Huruf ‘r’ sebelum String menganggap String sebagai raw
+
+  print('Hi \u2665'); //memasukkan unicode
+   */
+  //=========================================================
+
+  //============= Belajar Boolean =====================
+
+  bool alwaysTrue = true;
+  var alwaysFalse = false;
+  var notTrue = !true; //berarti artinya flase
+  bool notFalse = !false; //berarti true
+
+  int nilai1 = 11;
+  int nilai2 = 12;
+
+  if (nilai1 > nilai2) {
+    print("It's true");
+  } else {
+    print("It's False");
+  }
+
+  //=========================================================
 
 }
